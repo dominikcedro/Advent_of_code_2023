3e52509d769ba8387ee17d93d024cf318f14c1b7
Domin C <dominikcedro00@gmail.com>
2023-12-06 23:14:25 +0100

    Day 6 AoC finished task1, working on task2

---
+++ day6_task1.py
@@ -0,0 +1,65 @@
+import re
+import numpy as np
+def read_file_get_input(file):
+    matrix = np.empty((0, 4), int)
+    with open("day6_data.txt") as f:
+        data = f.read().splitlines()
+    for line in data:
+        numbers = re.findall(r'\d+', line)
+        numbers = [int(number) for number in numbers]
+        new_row = np.array(numbers)
+        matrix = np.vstack((matrix, new_row))
+    return matrix
+
+def get_pairs(matrix):
+    pairs = []
+    for k in range(0,4):
+        column = matrix[:, k].tolist()
+        pairs.append(column)
+    return pairs
+
+# def calculate_distance(distance, time):
+#     # i have distance
+#     # i need to calculate speed and rest of time
+#
+
+def calculate_possible(pairs):
+    possibles = []
+    for pair in pairs:
+
+        distance = pair[1]
+        time = pair[0]
+        button_time1 = 0
+        calculated_distance = 0
+        while calculated_distance <= distance:
+            calculated_distance = button_time1 * (time - button_time1)
+            button_time1 += 1
+        print(f"distance {calculated_distance} for button time {button_time1-1}")
+        range_begin = button_time1-1
+        # print(calculated_distance) # calculate distance is the lowest possible distance bigger then previous record
+
+        #now lets get the upper range
+        button_time2 = time
+        calculated_distance2 = 0
+        while calculated_distance2 <= distance:
+            calculated_distance2 = button_time2 * (time - button_time2)
+            button_time2 -= 1
+        print(f"distance {calculated_distance2} for button time {button_time2+1}")
+        range_end = button_time2+1
+        possible = range_end - range_begin +1
+
+        possibles.append(possible)
+    print(possibles)
+    result = 1
+    for k in possibles:
+        result *= k
+    print(result)
+
+def main():
+    matrix = read_file_get_input("day6_data.txt")
+    # print(matrix)
+    pairs = get_pairs(matrix)
+    calculate_possible(pairs)
+
+
+main()
