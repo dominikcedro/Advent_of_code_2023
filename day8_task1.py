7ac5a4de25696465c53ad68032b6dab719ef5a20
Domin C <dominikcedro00@gmail.com>
2023-12-09 19:29:33 +0100

    Days 7 & 8 Finished working on day 9

--- day8_task1.py
+++ day8_task1.py
@@ -34,28 +34,34 @@ def get_name(data):
             continue
     return dict_inst
 
-def direction_search(const_directions, directions, dict_inst, instruction, counter):
-    if len(directions) == 0:
-        print("end")
-        return counter
-    while len(directions) > 0:
-        # print(len(directions))
-        direction = int(directions[0])
-        directions = directions[1:]
-        new_inst = dict_inst[instruction][direction]
-        print(new_inst)
+def direction_search(directions, dict_inst, instruction, counter):
+    for direction in directions:
+        instruction = dict_inst[instruction][int(direction)]
         counter += 1
-        direction_search(const_directions, directions, dict_inst, new_inst, counter)
+        if instruction == "ZZZ":
+            print(counter)
+            return counter
+    if instruction != "ZZZ":
+        direction_search(directions, dict_inst, instruction, counter)
 
 
 
+
+    # if len(directions) == 0:
+    #     print("end")
+    #     return counter
+    # while len(directions) > 0:
+    #     # print(len(directions))
+    #     direction = int(directions[0])
+    #     directions = directions[1:]
+    #     new_inst = dict_inst[instruction][direction]
+    #     print(new_inst)
+    #     counter += 1
+    #     direction_search(const_directions, directions, dict_inst, new_inst, counter)
 
 def main():
     data = read_file("day8_data")
     directions = get_instructions(data)
     dict_inst = get_name(data)
-
-    counter = direction_search(directions, directions, dict_inst, "AAA", 0)
-    print(counter)
-
+    direction_search(directions, dict_inst, "AAA", 0)
 main()
